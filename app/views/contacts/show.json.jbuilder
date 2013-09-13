9dadc3218450c9b9ee153cb590e6b77db956375c
aulia <kingmalprax@gmail.com>
2013-09-13 11:03:55 +0800

    generate scaffold contact

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :name, :phone, :email, :address, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ContactBook::Application.routes.draw do
+  resources :contacts
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
