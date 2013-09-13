e45b09c4f1475e04d295c72a510338da53f83fb1
aulia <kingmalprax@gmail.com>
2013-09-13 10:26:09 +0800

    first step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactBook::Application.config.secret_key_base = 'c6c0596a5477b4ef41303ebde62d3664a9262d5492765aa79b281f4e4a61aa550fc6976f84b5c22763c1b50ab14100c84b930d26b88147365f35fcc30cb32e67'
