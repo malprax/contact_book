9ee6ff29bac23398afc34f9733fac3f760fb33e3
aulia <kingmalprax@gmail.com>
2014-01-02 22:24:02 +0800

    try to fix dropbox1

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,11 +2,15 @@ class Image < ActiveRecord::Base
   attr_accessible :title, :picture
   
   has_attached_file :picture, # :default_url => "/images/:style/missing.png"
+ ######## Ini upload yang disimpan di public assets########################### 
   :url => "/assets/images/:id/:style/:basename.:extension",
    :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
-    # :storage => :dropbox,
- #    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-     :styles => { :medium => "300x300" }
- #       , :dropbox_options => {
- #          :path => proc { |style| "#{style}/#{id}_#{picture.original_filename}" }}
+ ##############################################################################  
+   
+ ######## Ini upload yang disimpan di dropbox ################################
+  :storage => :dropbox,
+  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  :styles => { :medium => "300x300" }#, :dropbox_options => {
+  #:path => proc { |style| "#{style}/#{id}_#{picture.original_filename}" }}
+##############################################################################
 end
