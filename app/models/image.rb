5cbe4d1babcb8d2d16d9fc090f8f9a2b0b34dbe2
aulia <kingmalprax@gmail.com>
2014-01-03 10:18:25 +0800

    add cancan function

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,13 +4,14 @@ class Image < ActiveRecord::Base
   has_attached_file :picture, # :default_url => "/images/:style/missing.png"
  ######## Ini upload yang disimpan di public assets########################### 
   :url => "/assets/images/:id/:style/:basename.:extension",
-   :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
+  :styles => { :medium => "150x150" }
  ##############################################################################  
    
  ######## Ini upload yang disimpan di dropbox ################################
-  :storage => :dropbox,
-  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-  :styles => { :medium => "300x300" }#, :dropbox_options => {
-  #:path => proc { |style| "#{style}/#{id}_#{picture.original_filename}" }}
-##############################################################################
+  # :storage => :dropbox,
+#   :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+#   :styles => { :medium => "300x300" }, :dropbox_options => {
+#   :path => proc { |style| "#{style}/#{id}_#{picture.original_filename}" }}
+ #############################################################################
 end
