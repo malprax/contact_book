4f88b49625ef0b8a763f312ee5142b0424fc834e
aulia <kingmalprax@gmail.com>
2014-01-09 06:14:41 +0800

    try to fix dropdownmenu1

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,7 +4,6 @@ class Page < ActiveRecord::Base
   before_validation :generate_slug
   has_ancestry
   attr_writer :current_page
-  translates :name, :input_text, :slug, :ancestry
   
   
   def to_param
