a1b1c94a525fb30005fce52091b902150af98e3a
aulia <kingmalprax@gmail.com>
2014-01-27 13:01:22 +0800

    try to change font2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,8 @@ ContactBook::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+    config.assets.compile = true  
   
- 
  
  
 end
