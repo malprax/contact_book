a1d87f8fdbe76094d98b055a6b8fa69a840a11ae
aulia <kingmalprax@gmail.com>
2014-03-05 21:45:05 +0800

    editcss99

--- app/assets/javascripts/wysihtml5.js
+++ app/assets/javascripts/wysihtml5.js
@@ -1,7 +1,16 @@
 jQuery(document).ready(function(){
-
-   $('.wysihtml5').each(function(i, elem) {
-     $(elem).wysihtml5();
-   });
-
+         $('.wysihtml5').each(function(i, elem) {
+           $(elem).wysihtml5();
+         });
+      // $('#some-textarea').wysihtml5({
+//         "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+//         "emphasis": true, //Italics, bold, etc. Default true
+//         "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+//         "html": false, //Button which allows you to edit the generated HTML. Default false
+//         "link": true, //Button to insert a link. Default true
+//         "image": true, //Button to insert an image. Default true,
+//         "color": false //Button to change color of font  
+//       }); 
  })
+ 
+ 
