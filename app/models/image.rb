abb7e07ed9d6d89628129494ef8266583feeb723
aulia <kingmalprax@gmail.com>
2014-03-07 06:10:09 +0800

    editcss101

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,22 +1,22 @@
 class Image < ActiveRecord::Base
  attr_accessible :title, :picture 
- attr_accessor :title, :picture
+
  validates :title, presence: true
   
   has_attached_file :picture, 
-  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-  :url => "/system/:attachment/:id/:style/:filename",# :default_url => "/images/:style/missing.png"
+  # :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+#   :url => "/system/:attachment/:id/:style/:filename",# :default_url => "/images/:style/missing.png"
  ######## Ini upload yang disimpan di public assets########################### 
-  # :url => "/assets/images/:id/:style/:basename.:extension",
-#   :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
-#   :styles => { :medium => "150x150" }
+  :url => "/assets/images/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension",
+  :styles => { :medium => "150x150" }
  ##############################################################################  
    
  ######## Ini upload yang disimpan di dropbox ################################
-  :storage => :dropbox,
-  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-  :styles => { :medium => "50x50" }, 
-  :dropbox_options => {
-  :path => proc { |style| "#{style}/#{id}_#{picture.original_filename}" }}
+  # :storage => :dropbox,
+ #  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+ #  :styles => { :medium => "50x50" }, 
+ #  :dropbox_options => {
+ #  :path => proc { |style| "#{style}/#{id}_#{picture.original_filename}" }}
  #############################################################################
 end
