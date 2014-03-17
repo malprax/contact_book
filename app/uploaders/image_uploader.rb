3f071e718a7f4f436c8eb52017b9645449ed8542
aulia <kingmalprax@gmail.com>
2014-03-17 21:42:12 +0800

    modified uploader addpicture

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
-   storage :fog
+  storage :file
+   #storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -29,11 +29,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
    #def scale(width, height)
      # do something
-     #end
-     
-     include CarrierWave::MimeTypes
-      process :set_content_type
-
+     #end     
+    
   # Create different versions of your uploaded files:
    version :thumb do
       process :resize_to_limit => [50, 50]
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140308050549) do
 
   create_table "images", force: true do |t|
     t.string   "title"
+    t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
